b9e6c68cdb8a9a0109c1398866b73da759610e75
pretexxx <jorge.pretell@gmail.com>
2022-10-10 21:23:10 -0500

    agregacion: agrege los archivos de py

---
+++ dia-1/1-variables.py
@@ -0,0 +1,20 @@
+# esto es un comentario
+edad = 30
+
+nombre = 'eduardo'
+
+apellido ="gomez"
+
+despedida="""asdasdasd
+adasdasdasd
+asdasdasdsada
+"""
+
+especial = None
+curso,grupo,es,dia,nota='codigo','backedn','octubre',10,15.4
+
+print(nombre)
+print(despedida)
+print(especial)
+print(type(nombre))
+print(id(curso))
---
+++ dia-1/2-impresion-pantalla.py
@@ -0,0 +1,9 @@
+correo = 'asdad@gmail.com'
+
+estatura= 1.98
+
+print(correo)
+
+print('el correo del usuario es',correo)
+print('el correo del usuario es'+correo)
+print('la estatura es '+str(estatura))
